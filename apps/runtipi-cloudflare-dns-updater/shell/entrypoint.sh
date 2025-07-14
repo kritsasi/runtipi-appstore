89202b9ba1f2abf1d1fe35ff55155c4de0af4529
kritsasi <103875543+kritsasi@users.noreply.github.com>
2025-07-14 23:19:03 +0700

    Delete apps/runtipi-cloudflare-dns-updater/shell/entrypoint.sh

--- apps/runtipi-cloudflare-dns-updater/shell/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Use envsubst replace into shell script before run
-envsubst < /tmp/runtipi_cloudflare_dns_updater.template > /tmp/runtipi_cloudflare_dns_updater.sh
-
-chmod +x /tmp/runtipi_cloudflare_dns_updater.sh
-
-# Run shell script from generated script.
-exec /tmp/runtipi_cloudflare_dns_updater.sh
